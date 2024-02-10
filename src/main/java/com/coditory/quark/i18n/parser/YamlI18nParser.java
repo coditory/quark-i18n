95083fdb0612c823511d8e761eda09aa74bcc8e5
ogesaku <ogesaku@gmail.com>
2024-02-10 17:31:35 +0100

    Migrate to snakeyaml-engine

--- src/main/java/com/coditory/quark/i18n/parser/YamlI18nParser.java
+++ src/main/java/com/coditory/quark/i18n/parser/YamlI18nParser.java
@@ -3,7 +3,8 @@ package com.coditory.quark.i18n.parser;
 import com.coditory.quark.i18n.I18nKey;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.yaml.snakeyaml.Yaml;
+import org.snakeyaml.engine.v2.api.Load;
+import org.snakeyaml.engine.v2.api.LoadSettings;
 
 import java.util.Locale;
 import java.util.Map;
@@ -19,11 +20,12 @@ final class YamlI18nParser implements I18nParser {
         return I18nParsers.parseEntries(entries, locale);
     }
 
+    @SuppressWarnings("unchecked")
     private Map<String, Object> parseYaml(@NotNull String content) {
         try {
-            Yaml yaml = new Yaml(); // Yaml is not thread safe!
-            Map<String, Object> entries = yaml.load(content);
-            return entries == null || entries.isEmpty() ? Map.of() : entries;
+            LoadSettings settings = LoadSettings.builder().build();
+            Load yaml = new Load(settings);
+            return (Map<String, Object>) yaml.loadFromString(content);
         } catch (Throwable e) {
             throw new I18nParseException("Could not parse YAML", e);
         }
